80332720b1a135411e71652a197bdbc25735ee9b
riccoqu <ricco.qu@outlook.com>
2016-07-28 16:34:06 +0800

    MIMI类型

--- mime.go
+++ mime.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
+//此文件只定义了一个 mimemaps非导出变量
 package beego
 
 var mimemaps = map[string]string{
