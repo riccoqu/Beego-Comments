813e3aceafa0c5f2b7b3b26932d5085d2f01e567
riccoqu <ricco.qu@outlook.com>
2016-07-31 23:34:56 +0800

    控制器

--- controller.go
+++ controller.go
@@ -55,6 +55,8 @@ type ControllerComments struct {
 
 // Controller defines some basic http request handler operations, such as
 // http context, template and view, session and xsrf.
+//当用户需要自定义的 Controller是应该包含 Controller,因为 Controller实现了 Controller实现了 ControllerInterface接口
+//所以用户的自定义控制器可以被传入到 Router()函数中
 type Controller struct {
 	// context data
 	Ctx  *context.Context
@@ -84,6 +86,7 @@ type Controller struct {
 }
 
 // ControllerInterface is an interface to uniform all controller handler.
+//所有 Controller的通用接口
 type ControllerInterface interface {
 	Init(ct *context.Context, controllerName, actionName string, app interface{})
 	Prepare()
